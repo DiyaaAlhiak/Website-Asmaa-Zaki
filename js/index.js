c2110cc00bd25105de0429daf645fee916d8372f
DiyaaAlhiak <diyaahaik77@gmail.com>
2024-10-19 12:23:37 +0300

    chinge

--- js/index.js
+++ js/index.js
@@ -17,12 +17,18 @@ window.addEventListener('scroll', function() {
         elements.forEach(itme =>{
             itme.classList.add("active")
         })
-
-
-     
-   
     }
 });
 
-
-
+let TextCardID = document.querySelectorAll("#text-card1, #text-card2, #text-card3,#text-card4")
+let TextID1 = TextCardID[0]
+let TextID2 = TextCardID[1]
+let TextID3 = TextCardID[2]
+let TextID4 = TextCardID[3]
+function chinge(TextID1){
+    if(TextID1){
+        TextID1.classList.remove("hide")
+        TextID1.classList.add("show")
+        console.log("fffff")
+    } 
+}
